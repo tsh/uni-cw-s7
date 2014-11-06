a4ecdbe9be3d6c89d1719bb802eb28fe3902719c
Anton Nikulin <dr.tallin@gmail.com>
2014-11-06 18:44:40 +0200

    prl use tick for measure time elapsed

--- PrlProg/PrlProg/PrlProg.cpp
+++ PrlProg/PrlProg/PrlProg.cpp
@@ -6,6 +6,7 @@
 #include <emmintrin.h>
 #include <ctime>
 #include <Windows.h>
+#include <intrin.h>
 
 
 /* 26.	Дано 2 масиву чисел типу double однакової розмірності. Розробити алгоритм для обчислення z[i]= x[i]-y[i]. */
@@ -17,23 +18,27 @@ int _tmain(int argc, _TCHAR* argv[])
 	__m128d a;
 	double x[N], y[N], z[N];
 	int i;
-	int start_s=clock();
 	for (i=0; i<N; i++){
 		x[i]=rand();
 		y[i]=rand();
+	}
+
+	__int64 start_s=__rdtsc();
+	for (i=0; i<N; i++){
 		z[i] = x[i] - y[i];
 	}
-	int stop_s=clock();
+	__int64 stop_s=__rdtsc();
+
 	std::cout<<(stop_s -start_s)<<std::endl;
 
 	__m128d* X = (__m128d*)x;
 	__m128d* Y = (__m128d*)y;
 	__m128d* Z = (__m128d*)z;
-	int start_s2=clock();
-	for (i=0; i<N/8; ++i){
+	__int64 start_s2=__rdtsc();
+	for (i=0; i<N/2; ++i){
 		Z[i] = _mm_sub_pd(X[i], Y[i]);
 	}
-	int stop_s2=clock();
+	__int64 stop_s2=__rdtsc();
 	std::cout<<stop_s2 -start_s2;
 	getchar();
 	return 0;
