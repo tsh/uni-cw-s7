f84ff0b1afcc8bfd0a338de5f5a30f5088fabe61
Anton Nikulin <dr.tallin@gmail.com>
2014-11-06 12:29:46 +0200

    some dummy auctions view, controller

---
+++ TKPZ_kurs_1/TKPZ_kurs_1/Controllers/AuctionsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TKPZ_kurs_1.Controllers
+{
+    public class AuctionController : Controller
+    {
+        //
+        // GET: /Auctions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TempDataDemo()
+        {
+            TempData["SuccessMessage"] = "The action succeeded!";
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Auction()
+        {
+            var auction = new TKPZ_kurs_1.Models.Auction()
+            {
+                Title = "Example",
+                Description = "This is an example",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddDays(7),
+                StartPrice = 1.00m,
+                CurrentPrice = null,
+            };
+
+            return View(auction);
+        }
+
+    }
+}
