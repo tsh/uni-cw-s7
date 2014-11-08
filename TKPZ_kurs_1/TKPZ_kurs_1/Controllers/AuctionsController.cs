54fd2d42cf1748b179e925a972f2c8674dbf9d63
Anton Nikulin <dr.tallin@gmail.com>
2014-11-08 17:19:08 +0200

    bid

--- TKPZ_kurs_1/TKPZ_kurs_1/Controllers/AuctionsController.cs
+++ TKPZ_kurs_1/TKPZ_kurs_1/Controllers/AuctionsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TKPZ_kurs_1.Models;
@@ -57,6 +58,35 @@ namespace TKPZ_kurs_1.Controllers
                 return RedirectToAction("Index");
             }
             return Create();
+        }
+
+        [HttpPost]
+        public ActionResult Bid(Bid bid)
+        {
+            var db = new AuctionsDataContext();
+            var auction = db.Auctions.Find(bid.AuctionId);
+
+            if (auction == null)
+            {
+                ModelState.AddModelError("AuctionId", "Auction not found!");
+            }
+            else if (auction.CurrentPrice >= bid.Amount)
+            {
+                ModelState.AddModelError("Amount", "Bid amount must exceed current bid");
+            }
+            else
+            {
+                bid.Username = User.Identity.Name;
+                auction.Bids.Add(bid);
+                auction.CurrentPrice = bid.Amount;
+                db.SaveChanges();
+            }
+
+            if (!Request.IsAjaxRequest())
+                return RedirectToAction("Auction", new { id = bid.AuctionId });
+
+            var httpStatus = ModelState.IsValid ? HttpStatusCode.OK : HttpStatusCode.BadRequest;
+            return new HttpStatusCodeResult(httpStatus);
         }
     }
 
--- TKPZ_kurs_1/TKPZ_kurs_1/Models/Auction.cs
+++ TKPZ_kurs_1/TKPZ_kurs_1/Models/Auction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -42,6 +43,18 @@ namespace TKPZ_kurs_1.Models
         [DataType(DataType.Currency)]
         [Display(Name = "Current Bid Price")]
         public decimal? CurrentPrice { get; set; }
+
+        public virtual Collection<Bid> Bids { get; private set; }
+
+        public int BidCount
+        {
+            get { return Bids.Count; }
+        }
+
+        public Auction()
+        {
+            Bids = new Collection<Bid>();
+        }
 
     }
 }
